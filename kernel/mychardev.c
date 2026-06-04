f6f95ebf437eb1f5680dea68bc871933f2941ad2
Dang Luu Anh Tu <anhtu29126@gmail.com>
2026-06-04 23:40:18 +0700

    Add sysfs and mutex synchronization

--- kernel/mychardev.c
+++ kernel/mychardev.c
@@ -1,3 +1,4 @@
+#include <linux/mutex.h>
 #include <linux/ioctl.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -20,7 +21,7 @@ static struct device *my_device;
 
 static char kernel_buffer[BUFFER_SIZE];
 static size_t data_len = 0;
-
+static DEFINE_MUTEX(buffer_mutex);
 static int my_open(struct inode *inode, struct file *file)
 {
     pr_info("mychardev: device opened\n");
@@ -38,8 +39,13 @@ static ssize_t my_read(struct file *file, char __user *user_buf,
 {
     ssize_t bytes_to_read;
 
-    if (*ppos >= data_len)
+    if (mutex_lock_interruptible(&buffer_mutex))
+        return -ERESTARTSYS;
+
+    if (*ppos >= data_len) {
+        mutex_unlock(&buffer_mutex);
         return 0;
+    }
 
     bytes_to_read = data_len - *ppos;
 
@@ -47,6 +53,7 @@ static ssize_t my_read(struct file *file, char __user *user_buf,
         bytes_to_read = count;
 
     if (copy_to_user(user_buf, kernel_buffer + *ppos, bytes_to_read)) {
+        mutex_unlock(&buffer_mutex);
         pr_err("mychardev: failed to copy data to user\n");
         return -EFAULT;
     }
@@ -55,6 +62,7 @@ static ssize_t my_read(struct file *file, char __user *user_buf,
 
     pr_info("mychardev: sent %zd bytes to user\n", bytes_to_read);
 
+    mutex_unlock(&buffer_mutex);
     return bytes_to_read;
 }
 
@@ -68,9 +76,13 @@ static ssize_t my_write(struct file *file, const char __user *user_buf,
     if (bytes_to_write >= BUFFER_SIZE)
         bytes_to_write = BUFFER_SIZE - 1;
 
+    if (mutex_lock_interruptible(&buffer_mutex))
+        return -ERESTARTSYS;
+
     memset(kernel_buffer, 0, BUFFER_SIZE);
 
     if (copy_from_user(kernel_buffer, user_buf, bytes_to_write)) {
+        mutex_unlock(&buffer_mutex);
         pr_err("mychardev: failed to copy data from user\n");
         return -EFAULT;
     }
@@ -80,6 +92,7 @@ static ssize_t my_write(struct file *file, const char __user *user_buf,
 
     pr_info("mychardev: received %zu bytes from user\n", bytes_to_write);
 
+    mutex_unlock(&buffer_mutex);
     return bytes_to_write;
 }
 
@@ -89,13 +102,24 @@ static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
     switch (cmd) {
     case MYCHAR_IOC_CLEAR:
+        if (mutex_lock_interruptible(&buffer_mutex))
+            return -ERESTARTSYS;
+
         memset(kernel_buffer, 0, BUFFER_SIZE);
         data_len = 0;
+
+        mutex_unlock(&buffer_mutex);
+
         pr_info("mychardev: buffer cleared by ioctl\n");
         return 0;
 
     case MYCHAR_IOC_GET_LEN:
+        if (mutex_lock_interruptible(&buffer_mutex))
+            return -ERESTARTSYS;
+
         len = data_len;
+
+        mutex_unlock(&buffer_mutex);
 
         if (copy_to_user((int __user *)arg, &len, sizeof(len))) {
             pr_err("mychardev: failed to copy length to user\n");
@@ -110,6 +134,16 @@ static long my_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
         return -EINVAL;
     }
 }
+
+static ssize_t buffer_size_show(struct device *dev,
+                                struct device_attribute *attr,
+                                char *buf)
+{
+    return sprintf(buf, "%d\n", BUFFER_SIZE);
+}
+
+static DEVICE_ATTR_RO(buffer_size);
+
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = my_open,
@@ -155,6 +189,16 @@ static int __init mychardev_init(void)
         unregister_chrdev_region(dev_number, 1);
         return PTR_ERR(my_device);
     }
+   
+    ret = device_create_file(my_device, &dev_attr_buffer_size);
+    if (ret < 0) {
+        pr_err("mychardev: failed to create sysfs file\n");
+        device_destroy(my_class, dev_number);
+        class_destroy(my_class);
+        cdev_del(&my_cdev);
+        unregister_chrdev_region(dev_number, 1);
+        return ret;
+}
 
     memset(kernel_buffer, 0, BUFFER_SIZE);
     data_len = 0;
@@ -167,6 +211,7 @@ static int __init mychardev_init(void)
 
 static void __exit mychardev_exit(void)
 {
+    device_remove_file(my_device, &dev_attr_buffer_size);
     device_destroy(my_class, dev_number);
     class_destroy(my_class);
     cdev_del(&my_cdev);
